2c34bdbaf89112070603a5dddf31acdbf2cdb776
Zahra Khatibi <zahrakhatibiiii@gmail.com>
2024-11-24 12:46:19 +0100

    load data and check the head of them

---
+++ #hello world.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Load the data
+preferences = pd.read_excel(r"D:\github\x\dse\Python_Project\preferences.xlsx")
+guests = pd.read_excel(r"D:\github\x\dse\Python_Project\guests.xlsx")
+hotels = pd.read_excel(r"D:\github\x\dse\Python_Project\hotels.xlsx")
+
+# check data
+print(preferences.head())
+print(guests.head())
+print(hotels.head())
